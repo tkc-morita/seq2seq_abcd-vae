Corrected the weight of KL divergence for the shared variables.

clustering/learning.py
@@ -172,6 +172,7 @@ class Learner(object):
 		kl_loss = 0
 
 		num_batches = dataloader.get_num_batches()
+		num_strings = len(dataloader.dataset)
 
 		for batch_ix,(packed_input, is_offset, speaker, _) in enumerate(dataloader, 1):
 			packed_input = packed_input.to(self.device)
@@ -181,8 +182,8 @@ class Learner(object):
 			self.optimizer.zero_grad()
 
 			last_hidden = self.encoder(packed_input)
-			cluster_weights,kl_weight,_ = self.mixture_ratio_sampler(last_hidden)
-			features,kl_value,_ = self.mixture_components(cluster_weights, parameter_seed=last_hidden)
+			cluster_weights,kl_weight,_ = self.mixture_ratio_sampler(last_hidden, num_strings)
+			features,kl_value,_ = self.mixture_components(cluster_weights, num_strings, parameter_seed=last_hidden)
 			kl_loss_per_batch = kl_weight + kl_value
 			emission_loss_per_batch = []
 			end_prediction_loss_per_batch = []
@@ -206,7 +207,6 @@ class Learner(object):
 
 			logger.info('{batch_ix}/{num_batches} training batches complete.'.format(batch_ix=batch_ix, num_batches=num_batches))
 
-		num_strings = len(dataloader.dataset)
 		emission_loss /= num_strings
 		end_prediction_loss /= num_strings
 		kl_loss /= num_strings
@@ -233,6 +233,7 @@ class Learner(object):
 		sign_loss = 0
 
 		num_batches = dataloader.get_num_batches()
+		num_strings = len(dataloader.dataset)
 
 		with torch.no_grad():
 			for batch_ix, (packed_input, is_offset, speaker, _) in enumerate(dataloader, 1):
@@ -241,8 +242,8 @@ class Learner(object):
 				speaker = speaker.to(self.device)
 
 				last_hidden = self.encoder(packed_input)
-				cluster_weights,kl_weight,_ = self.mixture_ratio_sampler(last_hidden)
-				features,kl_value,_ = self.mixture_components(cluster_weights, parameter_seed=last_hidden)
+				cluster_weights,kl_weight,_ = self.mixture_ratio_sampler(last_hidden, num_strings)
+				features,kl_value,_ = self.mixture_components(cluster_weights, num_strings, parameter_seed=last_hidden)
 				kl_loss += kl_weight + kl_value
 				emission_loss_per_batch = []
 				end_prediction_loss_per_batch = []
@@ -254,7 +255,6 @@ class Learner(object):
 
 				logger.info('{batch_ix}/{num_batches} validation batches complete.'.format(batch_ix=batch_ix, num_batches=num_batches))
 
-		num_strings = len(dataloader.dataset)
 		emission_loss /= num_strings
 		end_prediction_loss /= num_strings
 		kl_loss /= num_strings

clustering/modules/model.py
@@ -487,7 +487,7 @@ class SampleFromDirichlet(torch.nn.Module):
 		self.to_non_negative = (lambda x: torch.nn.ELU()(x) + 1.0 + posterior_base_counts)
 
 
-	def forward(self, weights_seed):
+	def forward(self, weights_seed, entire_data_size):
 		# Sample a relaxed category assignment kappa from q(kappa | x) = Dirichlet(q_kappa_weights).
 		q_kappa_weights = self.to_non_negative(self.to_q_kappa_weights(weights_seed))
 		q_kappa_given_x = torch.distributions.dirichlet.Dirichlet(q_kappa_weights)
@@ -499,7 +499,7 @@ class SampleFromDirichlet(torch.nn.Module):
 		pi = q_pi.rsample(sample_shape=(kappa.size(0),))
 
 		# Compute the KL divergence between q(pi) and p(pi).
-		kl_divergence = torch.distributions.kl_divergence(q_pi, self.p_pi) / kappa.size(0)
+		kl_divergence = torch.distributions.kl_divergence(q_pi, self.p_pi) / entire_data_size
 
 		# Compute the KL divergence between q(kappa | x) and p(kappa | pi).
 		p_kappa_given_pi = torch.distributions.dirichlet.Dirichlet(self.relax_scalar * pi)
@@ -543,7 +543,7 @@ class SampleFromIsotropicGaussianMixture(torch.nn.Module):
 						requires_grad=True)
 					)
 
-	def forward(self, cluster_weights, parameter_seed=None):
+	def forward(self, cluster_weights, entire_data_size, parameter_seed=None):
 		# broadcast cluster_weights
 		cluster_weights = cluster_weights.view(cluster_weights.size()+(1,))
 		if self.post_mixture_noise: # Two-level Gaussian noise, one for each cluster, the other for post mixture.
@@ -567,6 +567,6 @@ class SampleFromIsotropicGaussianMixture(torch.nn.Module):
 			samples = (cluster_weights * samples).sum(1)
 
 			# Measure the KL divergence between q(z) and p(z).
-			kl_divergence = torch.distributions.kl_divergence(posterior_distr, self.prior_distr).sum() / cluster_weights.size(0)
+			kl_divergence = torch.distributions.kl_divergence(posterior_distr, self.prior_distr).sum() / entire_data_size
 			posterior_mean, posterior_log_var = self.posterior_mean, self.posterior_log_var
 		return samples, kl_divergence, (posterior_mean, posterior_log_var)